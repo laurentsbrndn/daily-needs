6be30ddc34eff30dcec4393b3a5cb52a67153af7
smndra <145784435+smndra@users.noreply.github.com>
2025-03-29 00:21:03 +0700

    first commit

--- resources/views/admin-sidebar/index.blade.php
+++ resources/views/admin-sidebar/index.blade.php
@@ -10,18 +10,45 @@
     <link rel="stylesheet" href="/css/admin-sidebar.css">
     <link rel="stylesheet" href="/css/admin-addproduct.css">
 </head>
-    
 
 <body>
     <div class="sidebar">
-        <h4 class="text-center">Daily Needs</h4>
-        <a href="/admin/myprofile">My Profile</a>
-        <a href="/admin/productlist">Product List</a>
-        <a href="/admin/transactions">Transactions</a>
-        <form action="/admin/logout" method="post">
-            @csrf
-            <button type="submit" class="dropdown-item">Logout</button>
-        </form>
+       
+        <div class="logo-container">
+            <img src="{{ asset('assets/image/Title Icon.jpeg') }}" alt="Logo" class="logo">
+
+        </div>
+        
+        <div class="sidebar-container">
+            <a href="/admin/myprofile" style="text-decoration: none; color: inherit;">
+                <div class = "menu-item">
+                    <i class="bi bi-person" style="color: #D1D88D;"></i>
+                    <span>Admin Profile</span>
+                </div>
+            </a>
+
+            <a href="/admin/productlist" style="text-decoration: none; color: inherit;">
+                <div class="menu-item">
+                    <i class="bi bi-archive" style="color: #D1D88D;"></i>
+                    <span>Product List</span>
+                </div>
+            </a>
+
+            <a href="/admin/transactions" style="text-decoration: none; color: inherit;">
+                <div class = "menu-item">
+                    <i class="bi bi-wallet2" style="color: #D1D88D;"></i>
+                    <span>Transactions</span>
+                </div>
+            </a>
+
+            <form action="/admin/logout" method="post" class="menu-item-logout">
+                @csrf
+                <i class="bi bi-box-arrow-left" style="color: #D1D88D;"></i>
+                <button type="submit" class="dropdown-item">Log Out</button>
+            </form>
+
+        </div>
+            
     </div>
 
     @yield('container')
