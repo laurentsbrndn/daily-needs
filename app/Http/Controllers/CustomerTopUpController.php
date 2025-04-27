1cb32a1dc3cd50ec3875751f479203268e7b89ee
JessicaDebora13 <jessdebora05@gmail.com>
2025-04-27 22:18:53 +0700

    coba debug by jess 27-apr-2025

--- app/Http/Controllers/CustomerTopUpController.php
+++ app/Http/Controllers/CustomerTopUpController.php
@@ -13,7 +13,9 @@ class CustomerTopUpController extends Controller
     public function show()
     {
         $customers = Auth::guard('customer')->user();
-        return view('topup.index', compact('customers'));
+        $paymentMethods = MsPaymentMethod::all(); 
+       // dd($paymentMethods); -> commenting by jess for test
+        return view('topup.index', compact('customers', 'paymentMethods'));
     }
 
     public function update(Request $request)
@@ -22,21 +24,24 @@ class CustomerTopUpController extends Controller
 
         $validateData = $request->validate([
             'customer_balance' => 'nullable|numeric|min:1',
+            'payment_method' => 'required|exists:ms_payment_methods,payment_method_id', //add by jess
         ]);
 
         if (empty($request->customer_balance)) {
-            return back()->withErrors(['customer_Balance' => 'Top up amount cannot be empty. Please enter the amount you wish to add.'])->withInput();
+            //ubah B jadi b di customer_balance
+            return back()->withErrors(['customer_balance' => 'Top up amount cannot be empty. Please enter the amount you wish to add.'])->withInput();
         }
 
         $customers->customer_balance += $validateData['customer_balance'];
 
         $customers->save();
+       // dd($request->all());  -> commenting by jess for test
 
         MsTopUp::create([
             'top_up_amount' => $validateData['customer_balance'],
             'top_up_date' => now(),
             'customer_id' => $customers->customer_id,
-            'payment_method_id' => 2,
+            'payment_method_id' => $request->payment_method,
         ]);
 
         $request->session()->flash('success', 'Your top up was successful! Your balance has been updated.');
--- resources/views/topup/index.blade.php
+++ resources/views/topup/index.blade.php
@@ -1,41 +1,66 @@
 @extends ('layouts.dashboardmain')
+<link rel="stylesheet" type="text/css" href="{{ asset('css/topup.css') }}">
 
 @section('container')
 
     <div class="content">
-        <h2>Top Up</h2>
-    
-        @if(session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
-        @endif
-
-        <div>
-            <h3>Balance</h3>
-            <h2>{{ $customers->customer_balance }}</h2>
+        <div class="wallet-bg">
+            <h2>Top Up</h2> <!-- Pindah ke dalam wallet-bg -->
         </div>
 
-        <form action="/dashboard/topup/update" method="post">
-            @csrf
-            @method('PUT')
+        <div class="wrapper">
+            @if(session('success'))
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
+            @endif
 
-            <div class="form-group">
-                <label>Top Up Amount</label>
-                <input type="number" name="customer_balance" class="form-control @error('customer_balance') is-invalid @enderror" value="{{ old('customer_balance')}}">
-                @error('customer_balance')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
+            <h3>Balance</h3>
+            <h2> Rp {{ number_format($customers->customer_balance, 0, ',', '.') }}</h2>
 
-            <div class="form-group">
-                <label for="payment_method">Payment Method</label>
-                <input type="text" name="payment_method" class="payment_method form-control" value="Bank Transfer" disabled>
-            </div>
+            <form action="/dashboard/topup/update" method="post">
+                @csrf
+                @method('PUT')
 
-            <button type="submit" class="btn btn-success">Top Up</button>
-        </form>
+                <div class="form-group">
+                    <label>Top Up Amount</label>
+                    <input type="number" name="customer_balance" class="form-control @error('customer_balance') is-invalid @enderror" value="{{ old('customer_balance')}}">
+                    @error('customer_balance')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <label for="payment_method">Payment Method</label>
+                    <select name="payment_method" id="payment_method" class="payment_method form-control">
+                        <option value="" disabled selected>Pilih Metode Pembayaran...</option>
+                        @foreach ($paymentMethods as $payment)
+                            <option value="{{ $payment->payment_method_id }}">{{ $payment->payment_name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <button type="submit" class="btn-success">Top Up</button>
+            </form>
+        </div>
     </div>
+
+   
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        let payment = document.getElementById("payment_method");
+        payment.selectedIndex = 0;
+
+        payment.addEventListener("change", function () {
+            let firstOption = this.querySelector("option[value='']");
+            
+            if (this.selectedIndex !== 0) {
+                firstOption.remove();
+            }
+        });
+    });
+</script>
+
+
 @endsection
