8c7c353437ef80293d40d48d1c66e98e2c72449f
ccccctj <tjoengchingtya@gmail.com>
2025-05-02 22:23:24 +0700

    customer-sidebar

--- resources/views/sidebar/index.blade.php
+++ resources/views/sidebar/index.blade.php
@@ -1,11 +1,46 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+<link rel="stylesheet" href="/css/sidebar.css">
+
 <div class="sidebar">
-    <h4 class="text-center">Daily Needs</h4>
-    <a href="/">Home</a>
-    <a href="/dashboard/myprofile">My Profile</a>
-    <a href="{{ route('purchasehistory.show', ['status' => 'Completed']) }}">Purchase History</a>
-    <a href="/dashboard/topup">Top Up</a>
-    <form action="/logout" method="post">
-        @csrf
-        <button type="submit" class="dropdown-item">Logout</button>
-    </form>
+    <div class="logo-container">
+        <img src="{{ asset('assets/image/Title Icon.jpeg') }}" alt="Logo" class="logo">
+
+    </div>
+
+    <div class="sidebar-container">
+        
+        <a href="/">
+            <div class = "menu-item">
+                <i class="bi bi-house-door" style="color: #D1D88D;"></i>
+                <span>Home</span>
+            </div>
+        </a>
+        
+        <a href="/dashboard/myprofile">
+            <div class = "menu-item">
+                <i class="bi bi-person" style="color: #D1D88D;"></i>
+                <span>My Profile</span>
+            </div>
+        </a>
+
+        <a href="{{ route('purchasehistory.show', ['status' => 'Completed']) }}">
+             <div class = "menu-item">
+                <i class="bi bi-clock-history" style="color: #D1D88D;"></i>
+                <span>Purchase History</span>
+            </div>
+        </a>
+        <a href="/dashboard/topup">
+            <div class = "menu-item">
+                <i class="bi bi-wallet2" style="color: #D1D88D;"></i>
+                <span>Top Up</span>
+            </div>
+        </a>
+    
+        <form action="/logout" method="post" class="menu-item-logout">
+            @csrf
+            <i class="bi bi-box-arrow-left" style="color: #D1D88D;"></i>
+            <button type="submit" class="dropdown-item">Log Out</button>
+        </form>
+    </div>
+
 </div>
